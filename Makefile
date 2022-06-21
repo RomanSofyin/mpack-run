0a9a93ccc4a0dad857f617b2e24a70e8c7746504
Roman Sofin <roma.sofin@gmail.com>
2022-06-21 21:30:10 +0300

    Add write example from mpack README.md

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: exe
 
 exe: ./src/main.c ./src/hexDump.c
-	 gcc ./src/main.c ./src/hexDump.c -o mpack-run
+	 gcc ./src/main.c ./src/hexDump.c mpack/mpack-common.c mpack/mpack-expect.c mpack/mpack-node.c mpack/mpack-platform.c mpack/mpack-reader.c mpack/mpack-writer.c -o mpack-run -I "mpack/"
 
 clean:
 	 -rm mpack-run 2>/dev/null
--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 #include "hexDump.h"
+#include "mpack.h"
 
 int main(int c, char **v)
 {
     // encode to memory buffer
     char* data;
     size_t size;
-    /*mpack_writer_t writer;
+    mpack_writer_t writer;
     mpack_writer_init_growable(&writer, &data, &size);
 
     // write the example on the msgpack homepage
@@ -21,19 +22,13 @@ int main(int c, char **v)
     // finish writing
     if (mpack_writer_destroy(&writer) != mpack_ok) {
         fprintf(stderr, "An error occurred encoding the data!\n");
-        return;
+        return -1;
     }
 
     // use the data
-    do_something_with_data(data, size);
-    free(data);*/
+    hexDump("--- MPACKed data ---", data, size);
+    free(data);
 
-    char test_str[] = "this is just a string to test how printf_data32() functions prints the data passed in";
-    hexDump("--- test_ints ---", test_str, sizeof(test_str));
-
-    char test_ints[] = { 0x12, 0x34, 0x56, 0x78, 0xfe, 0xed, 0xfa, 0xce, 0x0b, 0xad, 0xda, 0xce, 0x34, 0x56, 0x78, 0xfe, 0xed, 0xfa, 0xce, 0x0b, 0xad, 0xda, 0xce, 0x34, 0x56, 0x78, 0xfe, 0xed, 0xfa, 0xce, 0x0b, 0xad, 0xda, 0xce, 0x34, 0x56, 0x78, 0xfe, 0xed, 0xfa, 0xce, 0x0b, 0xad, 0xda, 0xce };
-    hexDump("--- test_ints ---", test_ints, sizeof(test_ints));
     printf("All is fine\n");
-
     return 0;
 }
