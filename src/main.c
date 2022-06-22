dc2deda81e5826a12fa69b4d764145bef64e9122
Roman Sofin <roma.sofin@gmail.com>
2022-06-22 13:55:41 +0300

    Add msgpack_homepage_example()

--- src/main.c
+++ src/main.c
@@ -3,32 +3,57 @@
 #include "hexDump.h"
 #include "mpack.h"
 
-int main(int c, char **v)
+int msgpack_homepage_example()
 {
     // encode to memory buffer
-    char* data;
-    size_t size;
-    mpack_writer_t writer;
-    mpack_writer_init_growable(&writer, &data, &size);
+    char *data = NULL;
+    size_t size = 0;
 
-    // write the example on the msgpack homepage
-    mpack_build_map(&writer);
-    mpack_write_cstr(&writer, "compact");
-    mpack_write_bool(&writer, true);
-    mpack_write_cstr(&writer, "schema");
-    mpack_write_uint(&writer, 0);
-    mpack_complete_map(&writer);
+    {
+        /* Encode the data using Write API */
+        mpack_writer_t writer;
+        mpack_writer_init_growable(&writer, &data, &size);
 
-    // finish writing
-    if (mpack_writer_destroy(&writer) != mpack_ok) {
-        fprintf(stderr, "An error occurred encoding the data!\n");
-        return -1;
+        // write the example on the msgpack homepage
+        mpack_build_map(&writer);
+        mpack_write_cstr(&writer, "compact");
+        mpack_write_bool(&writer, false);
+        mpack_write_cstr(&writer, "schema");
+        mpack_write_uint(&writer, 123);
+        mpack_complete_map(&writer);
+
+        // finish writing
+        if (mpack_writer_destroy(&writer) != mpack_ok)
+        {
+            fprintf(stderr, "An error occurred encoding the data!\n");
+            return -1;
+        }
     }
 
-    // use the data
+    // Print out the data MPACKed
     hexDump("--- MPACKed data ---", data, size);
-    free(data);
 
+    {
+        /* Decode the data MPACK'ed using Node API */
+        bool compact;
+        int schema;
+        mpack_tree_t tree;
+        mpack_tree_init_data(&tree, data, size);
+        mpack_tree_parse(&tree);
+
+        mpack_node_t root = mpack_tree_root(&tree);
+        compact = mpack_node_bool(mpack_node_map_cstr(root, "compact"));
+        schema = mpack_node_int(mpack_node_map_cstr(root, "schema"));
+        printf("Data read: compact=%s, schema=%d\n", compact ? "TRUE" : "FALSE", schema);
+    }
+
+    free(data);
+    return 0;
+}
+
+int main(int c, char **v)
+{
+    msgpack_homepage_example();
     printf("All is fine\n");
     return 0;
 }
